5ab280193ffbc6f8f98c6024b52185e44af0981e
Hugo Aparecido Gregorio de Almeida <hugoapga626@gmail.com>
2025-05-07 13:56:46 -0300

    Update triangulos.c

--- Tarefas/02/triangulos.c
+++ Tarefas/02/triangulos.c
@@ -6,13 +6,24 @@ Saída: Apresnetação da classificação do triângulo.
 
 #include <stdio.h>
 
-int main(void) {
-    
+int main(void)
+{
+
     printf("Digite dois ângulo de um triângulo em graus separados por espaço: ");
     char entrada[160];
-    fgets(entrada, sixeof(entrada), stdin);
+    fgets(entrada, sizeof(entrada), stdin);
     int angulo1, angulo2;
     sscanf(entrada, "%d %d", &angulo1, &angulo2);
-    
+
+    int soma_angulos = angulo1 + angulo2;
+    int angulo3 = 180 - soma_angulos;
+
+    if (angulo1 < 90 && angulo2 < 90 && angulo3 < 90)
+        printf("O triângulo é acutângulo.\n");
+    if (angulo1 == 90 || angulo2 == 90 || angulo3 == 90)
+        printf("O triângulo é retângulo.\n");
+    if (angulo1 > 90 || angulo2 > 90 || angulo3 > 90)
+        printf("O triângulo é obtusângulo.\n");
+
     return 0;
 }
